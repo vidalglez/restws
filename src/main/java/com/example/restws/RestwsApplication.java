7cae880950f98fa3439b8d0040755ab2f13a76c7
Vidal Gonzalez <darkvidal@gmail.com>
2018-07-23 21:58:32 -0500

    Implementing configurations for JPA, Hibernate and Spring boot

--- src/main/java/com/example/restws/RestwsApplication.java
+++ src/main/java/com/example/restws/RestwsApplication.java
@@ -1,12 +1,50 @@
 package com.example.restws;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.restws" })
+@EnableJpaRepositories(basePackages = "com.example.restws", entityManagerFactoryRef = "entityManagerFactory")
 public class RestwsApplication {
+
+	@Autowired
+	DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestwsApplication.class, args);
+	}
+
+	@Bean(name = "entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		// JpaVendorAdapteradapter can be autowired as well if it's configured in
+		// application properties.
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(false);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		// Add package to scan for entities.
+		factory.setPackagesToScan("com.example.restws");
+		factory.setDataSource(dataSource);
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory);
+		return txManager;
 	}
 }
